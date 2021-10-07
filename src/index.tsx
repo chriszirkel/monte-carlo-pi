c632116e1949f46fd8837afc74b0d9729788c238
Chris <chris.zirkel@gmail.com>
2021-10-07 10:25:31 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import createTheme from "@material-ui/core/styles/createTheme";
+import { ThemeProvider } from "@material-ui/styles";
+import { blue } from "@material-ui/core/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[900],
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/useDimensions.ts
+++ src/useDimensions.ts
@@ -1,14 +1,17 @@
 import React from "react";
 
-// https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
-// https://stackoverflow.com/questions/59873782/how-would-you-correctly-set-up-a-typescript-declaration-when-using-usecallback
+/**
+ * Get dimensions of HTML element.
+ *
+ * @see https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+ * @see https://stackoverflow.com/questions/59873782/how-would-you-correctly-set-up-a-typescript-declaration-when-using-usecallback
+ */
 export function useDimensions() {
-  const [dimensions, setDimensions] = React.useState<ClientRect | null>(null);
+  const [dimensions, setDimensions] = React.useState<DOMRect | null>(null);
 
   const ref = React.useCallback((node: HTMLElement | null) => {
     if (node !== null) {
       setDimensions(node.getBoundingClientRect());
-      console.log(node.getBoundingClientRect());
     }
   }, []);
 
